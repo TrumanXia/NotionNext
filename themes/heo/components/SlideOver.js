f3192f716ce0b3acf25726848775c5094e4ed45b
Truman <979944697@qq.com>
2024-05-09 16:56:04 +0800

    移动端去掉导航栏

--- themes/heo/components/SlideOver.js
+++ themes/heo/components/SlideOver.js
@@ -86,10 +86,11 @@ export default function SlideOver(props) {
                                             <section className='space-y-2 flex flex-col'>
                                                 <div>博客</div>
                                                 {/* 导航按钮 */}
-                                                <div className='gap-2 grid grid-cols-2'>
+                                              {/* alter  */}
+                                                {/* <div className='gap-2 grid grid-cols-2'>
                                                     <Button title={'主页'} url={'/'} />
                                                     <Button title={'关于'} url={'/about'} />
-                                                </div>
+                                                </div> */}
                                                 {/* 用户自定义菜单 */}
                                                 <MenuListSide {...props}/>
                                             </section>
